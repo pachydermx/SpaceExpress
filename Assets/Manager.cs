ab0d437a19c0b089b6ad6f5c1e15847e971361c6
PachydermX <renjimin@gmail.com>
2016-01-03 21:38:55 +0900

    Camera Switching

---
+++ Assets/Manager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	public Camera first_person_camera;
+	public Camera first_person_camera_reverse;
+
+	public Camera third_person_camera;
+	public Camera third_person_camera_reverse;
+
+	private Camera current_camera;
+
+	// Use this for initialization
+	void Start () {
+		current_camera = third_person_camera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Camera
+		if (Input.GetKeyDown(KeyCode.C)){
+			SwitchCamera();
+		}
+	}
+
+	void SwitchCamera () {
+		if (current_camera == this.first_person_camera){
+			first_person_camera.gameObject.SetActive(false);
+			third_person_camera.gameObject.SetActive(true);
+			current_camera = third_person_camera;
+		} else {
+			third_person_camera.gameObject.SetActive(false);
+			first_person_camera.gameObject.SetActive(true);
+			current_camera = first_person_camera;
+		}
+	}
+}
